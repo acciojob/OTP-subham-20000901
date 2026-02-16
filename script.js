843bdee049f6914eb9a10cb6a7ab89113b02ab5c
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-16 11:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let codes = document.querySelectorAll(".code");
+
+codes.forEach((code,index)) => {
+	code.addEventListener("input",function(){
+		if(code.innerText.length > 1 ){
+			code.innerText = code.innerText[0];
+		}
+		if(code.innerText.length === 1 && index < codes.length-1){
+			codes[index+1].focus();
+		}
+		
+	});
+
+	code.addEventListener("keydown",function(e){
+		if(e.key === "Backspace" && code.innerText === "" && index > 0){
+			codes[index-1].focus();
+		}
+	});
+}
